ea85ada55ef62dfc81e04ecf484b3e2f1f15f310
RakeanJati <muhamadrakean14@gmail.com>
2024-11-04 13:19:06 +0700

    Add files via upload

---
+++ Assets/js/data/profiles.js
@@ -0,0 +1,127 @@
+/*
+HOW TO SHOW IMAGE FROM GOOGLE DRIVE
+
+first, get your link
+https://drive.google.com/file/d/13O21HX86wUmbr8rs51RUwf3-af-XSOGB/view?usp=sharing
+
+then you'll get the ID of the file: 13O21HX86wUmbr8rs51RUwf3-af-XSOGB
+
+now paste it to this link
+https://drive.google.com/thumbnail?id={id}
+
+then you'll get this link for the image source, paste it in picture property on profiles object
+https://drive.google.com/thumbnail?id=13O21HX86wUmbr8rs51RUwf3-af-XSOGB
+
+*/
+
+const profiles =  [
+    {
+        no : 32,
+        
+        name : "ini si ratu",
+      
+        picture : "Assets/images/ratu.jpg"
+    },
+    {
+        no : 32,
+        
+        name : "ini si kean",
+      
+        picture : "Assets/images/12.jpeg"
+    },
+    {
+        no : 32,
+        
+        name : "ini si aul",
+      
+        picture : "Assets/images/aul.jpg"
+    },
+    {
+        no : 32,
+        
+        name : "ini waktu di pik nyari labubu",
+      
+        picture : "Assets/images/5.jpeg"
+    },
+    {
+        no : 32,
+        
+        name : "ini waktu di sharing terakhir upgrading sama 23",
+      
+        picture : "Assets/images/14.jpeg"
+    },
+    {
+        no : 32,
+        
+        name : "ibu ibu muslimah",
+      
+        picture : "Assets/images/7.jpeg"
+    },
+    {
+        no : 32,
+        
+        name : "guanya bagus",
+      
+        picture : "Assets/images/15.jpeg"
+    },
+    {
+        no : 32,
+        
+        name : "photobox patali!!!!",
+      
+        picture : "Assets/images/18.jpeg"
+    },
+    {
+        no : 32,
+        
+        name : "ngopi ganteng",
+      
+        picture : "Assets/images/13.jpeg"
+    },
+    {
+        no : 32,
+        
+        name : "estetik ceunah",
+      
+        picture : "Assets/images/17.jpeg"
+    },
+    {
+        no : 32,
+        
+        name : "apasih jempol doang",
+      
+        picture : "Assets/images/1.jpeg"
+    },
+    {
+        no : 32,
+        
+        name : "rambut guanya onpoint",
+      
+        picture : "Assets/images/9.jpeg"
+    },
+    {
+        no : 32,
+        
+        name : "mantai tapi gabisa nginjek pasir",
+      
+        picture : "Assets/images/8.jpeg"
+    },
+   
+   
+   
+   
+   
+    // {
+    //     no : null,
+    //     nis : "",
+    //     name : "",
+    //     nickname : "",
+    //     date_of_birth : "",
+    //     place_of_birth : "",
+    //     hobby : "",
+    //     quote : "",
+    //     message : "",
+    //     instagram : "",
+    //     picture : "https://drive.google.com/thumbnail?id={id}"
+    // },
+]
